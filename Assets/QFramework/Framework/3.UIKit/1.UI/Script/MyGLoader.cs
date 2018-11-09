7da7526b51fc0d6a7e9f2da76afb337bd7ecb6b0
DESKTOP-79SB44C\E_Ye <E_Ye@outlook.com>
2018-11-09 22:42:59 +0800

    no message

---
+++ Assets/QFramework/Framework/3.UIKit/1.UI/Script/MyGLoader.cs
@@ -0,0 +1,38 @@
+using FairyGUI;
+using System.Collections;
+using UnityEngine;
+
+namespace FairyGame
+{
+	public class MyGLoader : GLoader
+	{
+		public MyGLoader()
+		{
+		}
+
+		protected override void LoadExternal()
+		{
+			IconManager.inst.GetIcon(url, OnLoadCompleted);
+		}
+
+		protected override void FreeExternal(NTexture texture)
+		{
+			texture.refCount--;
+		}
+
+		private void OnLoadCompleted(string assetPath, string fileName, object data)
+		{
+			if (fileName != this.url) //Loader已经被另外设置了
+				return;
+
+			if (data != null)
+			{
+				NTexture texture = (NTexture)data;
+				texture.refCount++;
+				this.onExternalLoadSuccess(texture);
+			}
+			else
+				this.onExternalLoadFailed();
+		}
+	}
+}
